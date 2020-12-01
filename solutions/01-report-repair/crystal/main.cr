b121f20106723a565875db2937f3cc21fbc151b1
Amy <amy@fluff.tech>
2020-12-01 00:34:53 -0500

    whoops fix day 1 Crystal solution

--- solutions/01-report-repair/crystal/main.cr
+++ solutions/01-report-repair/crystal/main.cr
@@ -4,7 +4,7 @@ report = File.read_lines("input.txt").map(&.to_i)
 [2, 3].each do |count|
 	report.each_combination(count) do |combi|
 		if combi.sum == 2020
-			puts "Part #{x-1} answer: #{combi.product}"
+			puts "Part #{count-1} answer: #{combi.product}"
 			break
 		end
 	end
