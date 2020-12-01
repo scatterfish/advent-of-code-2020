2aa1602739760c5b62b4e3b1371693bec5ee1c67
Amy <amy@fluff.tech>
2020-12-01 00:24:50 -0500

    initial commit, day 1 Crystal solution

---
+++ solutions/01-report-repair/crystal/main.cr
@@ -0,0 +1,11 @@
+
+report = File.read_lines("input.txt").map(&.to_i)
+
+[2, 3].each do |count|
+	report.each_combination(count) do |combi|
+		if combi.sum == 2020
+			puts "Part #{x-1} answer: #{combi.product}"
+			break
+		end
+	end
+end
