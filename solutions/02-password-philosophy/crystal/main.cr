e37eec2f247b67b25bf4f0b404743e038b89f367
Amy <amy@fluff.tech>
2020-12-02 03:35:34 -0500

    day 2 Crystal solution

---
+++ solutions/02-password-philosophy/crystal/main.cr
@@ -0,0 +1,19 @@
+
+policies = File.read_lines("input.txt", chomp: true)
+
+count_valid = 0
+pos_valid = 0
+
+policies.each do |entry|
+	rules, code = entry.split(": ")
+	range, letter = rules.split(" ")
+	min, max = range.split("-").map(&.to_i)
+	
+	count_valid += 1 if min <= code.count(letter) <= max
+	
+	a, b = code[min - 1], code[max - 1]
+	pos_valid += 1 if a != b && [a, b].includes?(letter[0])
+end
+
+puts "Part 1 answer: #{count_valid}"
+puts "Part 2 answer: #{pos_valid}"
