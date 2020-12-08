4edc0e4d21d3efd16761a253c30025e062aa0288
Amy <amy@fluff.tech>
2020-12-07 20:37:53 -0500

    day 7 Crystal solution now that it's decent

---
+++ solutions/07-handy-haversacks/crystal/main.cr
@@ -0,0 +1,35 @@
+
+bag_rules = File.read_lines("input.txt").map(&.chomp("."))
+
+BAG_TABLE = Hash(String, Hash(String, Int32)).new
+
+bag_rules.each do |rule|
+	bag, contents = rule.gsub(/( bag)s?/, "").split(" contain ")
+	BAG_TABLE[bag] = contents.split(", ").map { |c|
+		unless c.ends_with?("no other")
+			{c[2..], c[0].to_i}
+		end
+	}.compact.to_h
+end
+
+def can_contain(target)
+	bags = Set(String).new
+	BAG_TABLE.each do |bag, contents|
+		if contents.keys.includes?(target)
+			bags << bag
+			bags += can_contain(bag)
+		end
+	end
+	return bags
+end
+
+def add_cost(bag)
+	costs = 0
+	BAG_TABLE[bag].each do |sub_bag, amount|
+		costs += amount * add_cost(sub_bag)
+	end
+	return costs + 1
+end
+
+puts "Part 1 answer: #{can_contain("shiny gold").size}"
+puts "Part 2 answer: #{add_cost("shiny gold") - 1}"
