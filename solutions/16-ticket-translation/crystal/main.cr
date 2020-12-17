15a83080792e08063810634eac1d67467bb9ef0d
Amy <amy@fluff.tech>
2020-12-16 22:08:37 -0500

    day 16 Crystal cleanup

--- solutions/16-ticket-translation/crystal/main.cr
+++ solutions/16-ticket-translation/crystal/main.cr
@@ -1,44 +1,34 @@
 
 rules, my_ticket, nearby = File.read("input.txt").split("\n\n").map(&.lines)
 
-# less than ideal parsing
-rules = rules.map { |rule|
-	field, values = rule.split(":")
-	values = values.split("or").map(&.split("-").map(&.to_i))
-	ranges = values.map { |(min, max)| (min..max) }
-	{field, ranges}
-}.to_h
-my_ticket = my_ticket[1].split(",").map(&.to_i64)
-nearby    = nearby[1..].map(&.split(",").map(&.to_i))
+rules = rules.to_h { |rule| # don't try to read into this one
+	vals = rule.scan(/(.+):|(\d+)-(\d+)/).compact_map(&.captures.compact)
+	{vals.shift.first, vals.map(&.map(&.to_i)).map{ |(min, max)| min..max }}
+}
+tickets = (my_ticket + nearby).compact_map { |ticket|
+	ticket.split(",").map(&.to_i64) unless ticket.matches?(/[a-z]/)
+}
 
-invalid_vals = Array(Int32).new
+invalid_vals = Array(Int64).new
 
-nearby.flatten.each do |val|
-	tests = rules.map { |_, ranges|
-		ranges.map { |range|
-			range.covers?(val)
-		}.any?
+tickets.flatten.each do |val|
+	tests = rules.values.map { |ranges|
+		ranges.any?(&.covers?(val))
 	}
 	invalid_vals << val unless tests.any?
 end
 
 puts "Part 1 answer: #{invalid_vals.sum}"
 
-nearby.reject! { |ticket|
-	ticket.map { |val|
-		invalid_vals.includes?(val)
-	}.any?
-}
+tickets.reject! { |ticket| (ticket & invalid_vals).any? }
 
 fields_map = Hash(String, Int32).new
-candidates = rules.keys.map{ |field| {field, Array(Int32).new} }.to_h
+candidates = rules.keys.to_h { |field| {field, Array(Int32).new} }
 
 rules.each do |field, ranges|
 	(0...rules.size).each do |i|
-		tests = nearby.map(&.[i]).map { |val|
-			ranges.map { |range|
-				range.covers?(val)
-			}.any?
+		tests = tickets.map(&.[i]).map { |val|
+			ranges.any?(&.covers?(val))
 		}
 		candidates[field] << i if tests.all?
 	end
@@ -47,13 +37,14 @@ end
 while fields_map.size < candidates.size
 	candidates.each do |field, positions|
 		if positions.size == 1
-			pos = positions[0]
+			pos = positions.first
 			fields_map[field] = pos
-			candidates.values.map(&.delete(pos))
+			candidates.values.each(&.delete(pos))
 		end
 	end
 end
 
 target_fields = rules.keys.select(&.starts_with?("departure"))
-target_vals = target_fields.map { |field| my_ticket[fields_map[field]] }
+target_vals   = target_fields.map { |field| tickets.first[fields_map[field]] }
+
 puts "Part 2 answer: #{target_vals.product}"
